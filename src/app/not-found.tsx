d3b36bd96adac2a046250cec6bfe8da3e30640d0
kiineld <yaromyr.akimyv@gmail.com>
2024-10-22 23:59:07 +0300

    link_instead_of_a

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import {buttonVariants} from "@/components/ui";
 import {Separator} from "@/components/ui";
 import {ArrowLeft} from "lucide-react";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
@@ -8,12 +9,12 @@ export default function NotFound() {
             <h1 className="font-bold text-[12rem] text-primary">404</h1>
             <h2 className="font-bold text-6xl mb-5">Страница не найдена</h2>
             <p className="font-medium text-xl mb-5">Страница, которую вы ищете, не существует</p>
-            <a href="/"
+            <Link href="/"
                className={buttonVariants({variant: "default", className: "group relative gap-2"})}>
                 <ArrowLeft className="group-hover:-translate-x-2 transition"/>
                 <Separator orientation="vertical"/>
                 Вернуться
-            </a>
+            </Link>
         </div>
     )
 }
